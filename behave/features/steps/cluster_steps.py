13742dc57967ac4c281bd6d837d40a4fd0971d3f
vrovachev <vrovachev@mirantis.com>
2013-03-18 17:34:26 +0400

    changes behave tests

--- behave/features/steps/cluster_steps.py
+++ behave/features/steps/cluster_steps.py
@@ -22,9 +22,6 @@ def get_clusters(context):
     status_code = res.status_code
     if status_code == 200:
         res_content_list_clusters = json.loads(res.content)
-        loop = res_content_list_clusters['clusters']
-        for x in loop:
-            cluster_ids.append(x.pop(u'id'))
 
 @When ('User see cluster with id:"{n}"')
 def get_cluster(context, n):
@@ -36,16 +33,16 @@ def get_cluster(context, n):
     if status_code == 200:
         res_content_get_cluster = json.loads(res.content)
 
-@When('User add cluster name:"{name}" and image_id:"{im_id}"')
-def create_cluster_body(context, name, im_id):
+@When('User add cluster name:"{name}" | image_id:"{im_id}" | n_n:"{n_n}" count_n_n:"{count_n_n}" | d_n:"{d_n}" count_d_n:"{count_d_n}"')
+def create_cluster_body(context, name, im_id,n_n, count_n_n, d_n, count_d_n):
     global cluster_body
     data=json.dumps(dict(
         cluster = dict(
             name = '%s' % (name),
             base_image_id = '%s' % (im_id),
             node_templates = {
-                'jt_nn.small': 1,
-                'tt_dn.small': 1
+                '%s': '%d' % (n_n) %(count_n_n),
+                '%s': '%d' % (d_n) %(count_d_n)
             }
         )))
     cluster_body = data
@@ -58,9 +55,9 @@ def add_cluster(context):
     res = rest.create_cluster(cluster_body)
     #sleep(60)
     status_code = res.status_code
-    #if status_code == 202:
-    #    res_content = eval(res.content)
-    #    cluster_ids.append(res_content.get(u'id'))
+    if status_code == 202:
+        res_content = json.loads(res.content)
+        cluster_ids.append(res_content.get(u'id'))
     
 @When ('User delete cluster with id:"{n}"')
 def del_cluster(context, n):
@@ -72,16 +69,8 @@ def del_cluster(context, n):
 def put_cluster(context, n):
     global status_code
     global res_content
-    data=json.dumps(dict(
-        cluster = dict(
-            name = 'QA_cluster_%d' % randint(0,100),
-            base_image_id = 'd9342ba8-4c51-441c-8d5b-f9e14a901299',
-            node_templates = {
-                'jt_nn.medium': 1,
-                'tt_dn.small': 5
-            }
-        )))
-    res=rest.create_cluster(data, cluster_ids[n])
+    global cluster_body
+    res=rest.create_cluster(cluster_body, cluster_ids[n])
     status_code = res.status_code
     if status_code == 202:
-        res_content = eval(res.content)
+        res_content = json.loads(res.content)
